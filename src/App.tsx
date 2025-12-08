46d06b5b7a93fd5fc16be4df2a02d4be8626ebba
hjj <hjj5946@gmail.com>
2025-12-08 12:44:35 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-black text-white min-h-screen flex items-center justify-center text-3xl">
+      Tailwind OK
+    </div>
+  )
+}
+
+export default App
